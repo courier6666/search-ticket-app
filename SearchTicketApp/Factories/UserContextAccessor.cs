2c3f8e9d903c86f780c63b41aa27844c4eae2d61
courier6666 <petzik.popovich@gmail.com>
2025-11-14 12:51:06 +0200

    added factories and helpers

---
+++ SearchTicketApp/Factories/UserContextAccessor.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using SearchTicketApp.Interfaces;
+using SearchTicketApp.Models.User;
+
+namespace SearchTicketApp.Factories
+{
+    public class UserContextAccessor : IUserContextAccessor
+    {
+        private const string UserContextCookieName = "userContext";
+
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly JsonSerializerOptions jsonOptions;
+
+        public UserContextAccessor(IHttpContextAccessor httpContextAccessor, JsonSerializerOptions jsonOptions)
+        {
+            ArgumentNullException.ThrowIfNull(httpContextAccessor);
+            ArgumentNullException.ThrowIfNull(jsonOptions);
+
+            this.httpContextAccessor = httpContextAccessor;
+            this.jsonOptions = jsonOptions;
+        }
+
+        public UserContext? GetUserContext()
+        {
+            var cookieValue = GetUserContextCookie();
+            if (cookieValue is null)
+                return null;
+
+            try
+            {
+                return JsonSerializer.Deserialize<UserContext>(cookieValue, jsonOptions);
+            }
+            catch (JsonException)
+            {
+                return null;
+            }
+        }
+
+        private string? GetUserContextCookie()
+        {
+            return httpContextAccessor.HttpContext?
+                .Request
+                .Cookies[UserContextCookieName];
+        }
+    }
+
+}
---
+++ SearchTicketApp/Helpers/DateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SearchTicketApp.Helpers
+{
+    public static class DateTimeConverter
+    {
+        private static string GetTimeZoneWindowsId(string timeZoneIaanId)
+        {
+            if (!TimeZoneInfo.TryConvertIanaIdToWindowsId(timeZoneIaanId, out string? windowsId))
+            {
+                throw new InvalidOperationException($"Cannot convert such Iaan id '{timeZoneIaanId}' to windows id.");
+            }
+
+            return windowsId!;
+        }
+
+        public static DateTime ToLocal(DateTime utcTime, string timeZoneId)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(utcTime,TimeZoneInfo.FindSystemTimeZoneById(GetTimeZoneWindowsId(timeZoneId)));
+        }
+
+        public static DateTime ToUtc(DateTime localTime, string timeZoneId)
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(localTime, TimeZoneInfo.FindSystemTimeZoneById(GetTimeZoneWindowsId(timeZoneId)));
+        }
+    }
+}
